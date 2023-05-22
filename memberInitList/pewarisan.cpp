a93808a3b68fb649e661b87694e1d909910a8e6c
azzamaflah <azzamhebat46@gmail.com>
2023-05-22 16:09:30 +0700

    dideklarasikan sebagai konstanta

--- memberInitList/pewarisan.cpp
+++ memberInitList/pewarisan.cpp
@@ -33,4 +33,16 @@ public:
 		~pelajar() {
 			cout << "pelajar dihapus\n" << endl;
 		}
+
+		string perkenalan() {
+			return "hallo, nama saya " + nama + " dari sekolah " + sekolah + "n\n";
+		}
 	};
+
+	int main() {
+		pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+		cout << siswa1.perkenalan();
+		cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+		return 0;
+	}
