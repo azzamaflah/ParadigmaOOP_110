7bda7e0f9c2e07f7dc03713cbab2e8a196cc0570
azzamaflah <azzamhebat46@gmail.com>
2023-05-22 11:35:43 +0700

    membuat variable dan method

--- memberInitList/pewarisan.cpp
+++ memberInitList/pewarisan.cpp
@@ -19,3 +19,18 @@ public:
 
 	}
 };
+
+	class pelajar : public orang { 
+	public:
+		string sekolah;
+
+		pelajar(string pNama, string pSekolah):
+		orang(pNama),
+			sekolah(pSekolah) {
+			cout << "pelajar dibuat\n" << endl;
+
+			}
+		~pelajar() {
+			cout << "pelajar dihapus\n" << endl;
+		}
+	};
